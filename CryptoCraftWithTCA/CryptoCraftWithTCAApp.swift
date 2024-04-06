5f127283d3b100fe596f2aefdd6321f10621945c
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2024-04-06 16:49:01 +0900

    first commit

---
+++ CryptoCraftWithTCA/CryptoCraftWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCraftWithTCAApp.swift
+//  CryptoCraftWithTCA
+//
+//  Created by SHIRAISHI HIROYUKI on 2024/04/06.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCraftWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
